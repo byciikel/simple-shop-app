40176583472e92ad4f11fae906a67ad4c7645191
Bayu Utomo <by.ciikel@gmail.com>
2020-03-18 16:54:52 +0700

    Updates

---
+++ precache-manifest.9b6403cff3b8c37589cd097cd8a49850.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7a234e6d61b81a5c3ac4a3b898e5793",
+    "url": "/simple-shop-app/index.html"
+  },
+  {
+    "revision": "e2c70f0e4adffdc3ada8",
+    "url": "/simple-shop-app/static/css/main.f5764744.chunk.css"
+  },
+  {
+    "revision": "02bef7302616764783cf",
+    "url": "/simple-shop-app/static/js/2.8538cd1d.chunk.js"
+  },
+  {
+    "revision": "b2dd832b2acc0a7fd9cc36f19837ab9d",
+    "url": "/simple-shop-app/static/js/2.8538cd1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2c70f0e4adffdc3ada8",
+    "url": "/simple-shop-app/static/js/main.bd55aeec.chunk.js"
+  },
+  {
+    "revision": "52a4a96d7160d92b69a7",
+    "url": "/simple-shop-app/static/js/runtime-main.f6ebc64b.js"
+  }
+]);
